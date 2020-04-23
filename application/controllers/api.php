f2511a765e2520a37ce061048fcd8b19e3f4b5b4
mdjamilaj <mdjamilaj1@gmail.com>
2020-04-23 10:05:41 +0600

    add multiple id pass

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -15,7 +15,7 @@ class api extends CI_Controller
             $this->db->order_by("date_time", "asc");
             $result = $this->db->get('class_routine')->result_array();
 
-            for($i=0; $i<count($result); $i++){
+            for ($i = 0; $i < count($result); $i++) {
                 $class_id =  $result[$i]['class_id'];
                 $sub_id =  $result[$i]['sub_id'];
                 $class = $this->db->where('class_id', $class_id)->get('class_list')->row();
@@ -45,8 +45,8 @@ class api extends CI_Controller
 
         $date = date("Y-m-d");
         $nextdate = date('Y-m-d', strtotime(' +1 day'));
-        $datetime = $date. ' 00:00';
-        $nextdatetime = $nextdate . ' 00:00'; 
+        $datetime = $date . ' 00:00';
+        $nextdatetime = $nextdate . ' 00:00';
 
         if ($token == 'ynT6AmjW') {
 
@@ -85,7 +85,7 @@ class api extends CI_Controller
         $token = $this->uri->segment(4);
         $limit = $this->uri->segment(3);
 
-        if($token == 'ynT6AmjW'){
+        if ($token == 'ynT6AmjW') {
 
             $this->db->order_by("date_time", "asc");
             $this->db->limit($limit, 0);
@@ -108,7 +108,7 @@ class api extends CI_Controller
 
             $data['result'] = $result;
             echo json_encode($data);
-        }else{
+        } else {
             $data['result'] = "Token Does't Matched";
             echo json_encode($data);
         }
@@ -267,7 +267,7 @@ class api extends CI_Controller
 
     #http://bdroutine.jamil.onlinehost.itlab.solutions/api/classIdToFetchSub/Class_ID/TOKEN
     #http://bdroutine.jamil.onlinehost.itlab.solutions/api/classIdToFetchSub/1/ynT6AmjW
-    
+
 
 
     public function classIdToFetchSub()
@@ -307,7 +307,7 @@ class api extends CI_Controller
             $this->db->order_by("id", "desc");
             $this->db->where('date_time >=', $datetime);
             $this->db->where('class_id', $class_id);
-            if($sub_id != 0){
+            if ($sub_id != 0) {
                 $this->db->where('sub_id', $sub_id);
             }
             $result = $this->db->get('class_routine')->result_array();
@@ -347,15 +347,19 @@ class api extends CI_Controller
         $nextdate = date('Y-m-d', strtotime(' +1 day'));
         $datetime = $date . ' 00:00';
         $nextdatetime = $nextdate . ' 00:00';
-        // echo $datetime;
-        // echo $nextdatetime;
 
         if ($token == 'ynT6AmjW') {
 
             $this->db->order_by("id", "desc");
             $this->db->where('date_time >=', $datetime);
             $this->db->where('date_time <=', $nextdatetime);
-            $this->db->where('class_id', $class_id);
+            if (strpos($class_id, '-') !== false) {
+
+                $arr = explode("-", $class_id);
+                $this->db->where_in('class_id', $arr);
+            } else {
+                $this->db->where('class_id', $class_id);
+            }
             $result = $this->db->get('class_routine')->result_array();
 
             for ($i = 0; $i < count($result); $i++) {
@@ -402,7 +406,6 @@ class api extends CI_Controller
 
                 $arr = explode("-", $class_id);
                 $this->db->where_in('class_id', $arr);
-                
             } else {
                 $this->db->where('class_id', $class_id);
             }
@@ -440,8 +443,4 @@ class api extends CI_Controller
         echo "<script>setInterval(function () { console.log('call me!');";
         echo "}, 200);</script>";
     }
-
-
-
-
 }
